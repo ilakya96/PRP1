306a3290d48463d96d7ab68b842d30f19898d49b
ilakya96 <ilakyaselvi1996@gmail.com>
2017-06-29 12:24:57 +0530

    Create Numbers.java

---
+++ Numbers.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Numbers
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a%2==0)
+{
+System.out.println("Even");
+}
+else
+System.out.println("Odd");
+}
+}
