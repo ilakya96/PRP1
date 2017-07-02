1b0a600d5ab077eb0ed24bd6d3e6a12cfd86326a
ilakya96 <ilakyaselvi1996@gmail.com>
2017-07-02 00:46:03 -0700

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,10 @@
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+if(n>0)
+  System.out.print("positive");
+  else if(n<0)
+  System.out.println("negative");
+  else
+  System.out.println("zero");
+  
